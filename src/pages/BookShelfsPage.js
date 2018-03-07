77301530f737f8f9f14696082fece3c3a0f407c7
Mihai Mijea <mijeamihai@yahoo.com>
2018-03-07 11:18:49 +0100

    feature: add shelfs types to constants

--- src/pages/BookShelfsPage.js
+++ src/pages/BookShelfsPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import BookShelf from '../components/BookShelf/BookShelf';
-import { titles } from '../utils/constants';
+import { shelfs, shelfsTitles } from '../utils/constants';
 
 const BookShelfsPage = ({ books = [], moveToShelf }) => {
-  const shelfs = books.map(book => book.shelf).filter((item, index, self) => self.indexOf(item) === index);
+
   return (
     <div className='list-books'>
       <div className='list-books-title'>
@@ -12,7 +12,7 @@ const BookShelfsPage = ({ books = [], moveToShelf }) => {
       </div>
       <div className='list-books-content'>
         <div>
-          {shelfs.map(shelf => <BookShelf key={shelf} title={titles[shelf]} books={books.filter(book => book.shelf === shelf)} moveToShelf={moveToShelf} />)}
+          {shelfs.map(shelf => <BookShelf key={shelf} title={shelfsTitles[shelf]} books={books.filter(book => book.shelf === shelf)} moveToShelf={moveToShelf} />)}
         </div>
       </div>
       <div className='open-search'>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
-export const titles = {
-  currentlyReading: 'Currently Reading',
-  wantToRead: 'Want To Read',
-  read: 'Read'
+export const shelfs = ['currentlyReading', 'wantToRead', 'read'];
+export const shelfsTitles = {
+  [shelfs[0]]: 'Currently Reading',
+  [shelfs[1]]: 'Want To Read',
+  [shelfs[2]]: 'Read'
 }
